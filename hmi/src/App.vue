5b016e791336b53313a5a5c343a8def3fdc8260f
David LaJambe <dlajambe@gmail.com>
2024-10-06 14:32:54 -0400

    Refactor CSS into SASS

--- hmi/src/App.vue
+++ hmi/src/App.vue
@@ -4,6 +4,38 @@
 
 <template>
   <header>
-    <h1>My bad</h1>
+    <h1 class="n1">Neutral 1</h1>
+    <h1 class="n2">Neutral 2</h1>
+    <h1 class="n3">Neutral 3</h1>
+    <h1 class="n4">Neutral 4</h1>
+    <h1 class="n5">Neutral 5</h1>
+    <h1 class="white">White</h1>
   </header>
 </template>
+
+<style scoped lang="scss">
+@import 'styles/abstracts/variables';
+
+.n1 {
+  color: $c-neutral-100;
+}
+.n2 {
+  color: $c-neutral-200;
+}
+.n3 {
+  color: $c-neutral-300;
+}
+.n4 {
+  color: $c-neutral-400;
+}
+.n5 {
+  color: $c-neutral-500;
+}
+
+.white {
+  color:#D9D9D9;
+}
+
+
+
+</style>
