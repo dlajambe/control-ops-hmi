377784b3b89ffae50d89f45e6ed6d028c5b016a2
David LaJambe <dlajambe@gmail.com>
2024-10-06 20:18:32 -0400

    Improve neutral colour palette

--- hmi/src/views/ControlRoom.vue
+++ hmi/src/views/ControlRoom.vue
@@ -5,10 +5,31 @@ import Sidebar from '@/components/Sidebar.vue'
 
 <template>
   <div class="control-room">
-    <Sidebar />
+    <div class="sidebar">
+      <Sidebar />
+    </div>
+    <div class="display">
+      <h1>Display</h1>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 @import '../styles/abstracts/variables';
+
+.control-room {
+  display: flex;
+  flex-direction: row;
+  color: $c-neutral-500;
+
+  .sidebar {
+    background-color: $c-neutral-100;
+  }
+
+  .display {
+    background-color: $c-neutral-200;
+  }
+}
+
+
 </style>
